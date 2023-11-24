1b6223e058096ad6e101e6ed25f7ab46dc8f16fb
Wallens1 <145782388+Wallens1@users.noreply.github.com>
2023-11-23 19:55:19 -0500

    Add files via upload

--- proyectoFinal.py
+++ proyectoFinal.py
@@ -1,11 +1,15 @@
 import os
 
 archivoUsuarios = None
+usuarioActual = None
+
 
 def linea():
     print("------------------------")
 
 def menuInicial():
+
+    global usuarioActual
 
     print("\n-----Menu Principal-----")
     print("1. Registrarse")
@@ -58,13 +62,12 @@ def cargarPlatos():
 def cargarMesas():
     pass
 
-
 #Menu inicial
 def registrarse():
     
     gmail = input("Ingrese su correo: ")
     
-    while(True):
+    if(buscarUsuario(gmail) == False):
         if(verificarCorreo(gmail)):
             print("Correo valido")
             contraseña = input("Ingrese una contraseña: ")
@@ -74,9 +77,14 @@ def registrarse():
                 linea()
                 guardarUsuario(gmail, contraseña)
                 menuInicial()
-                break
         else:
             print("Este correo no cuenta con los requisitos minimos\nIntente denuevo")  
+            registrarse()
+    else:
+        print("Este correo ya esta registrado, intentelo de nuevo")
+        registrarse()
+            
+
     
 def verificarCorreo(gmail):
 
@@ -88,7 +96,6 @@ def verificarCorreo(gmail):
     return False
 
 def verificarContraseña(contraseña):
-
     return True
 
 def guardarUsuario(correo, contraseña):
@@ -98,23 +105,87 @@ def guardarUsuario(correo, contraseña):
     with open(archivoUsuarios.name, "a+") as f:
         f.write(f"{correo},{contraseña}\n")
         f.flush()
-
-    with open(archivoUsuarios.name, "r") as f:
-        contenido = f.read()
  
 
 def iniciarSesion():
-    nombre = input("Ingrese su nombre de usuario: ")
-    buscarUsuario(nombre)
-    
 
-def buscarUsuario(nombre):
-    pass
+    global usuarioActual
+
+    nombre = input("Ingrese su nombre de usuario: ")
+    if(buscarUsuario(nombre)):
+        contraseña = input("Ingrese su clave: ")
+        if(confirmarContraseña(nombre, contraseña)):
+            print("Sesion iniciada correctamente.")
+            usuarioActual = nombre
+            menuPrincipal()
+        else:
+            print("Contraseña incorrecta, intente de nuevo")
+            iniciarSesion()
+    else:
+        print("Nombre de usuario incorrecto, intente de nuevo")
+        iniciarSesion()
+
+    
+def confirmarContraseña(usuario, contraseña):
+
+    global archivoUsuarios
+
+    if archivoUsuarios:
+        if(buscarUsuario(usuario)):
+            archivoUsuarios.seek(0)
+            for linea in archivoUsuarios:
+                usuarioA, contraseñaA = linea.strip().split(',')
+                if contraseña == contraseñaA and usuario == usuarioA:
+                    return True
+            return False
+        else:
+            return False
+    else:
+        print("Error: El archivo de usuarios no está abierto.")
+
+def buscarUsuario(usuario):
+    
+    global archivoUsuarios
+
+    if archivoUsuarios:
+        archivoUsuarios.seek(0)
+        for linea in archivoUsuarios:
+            gmail, contraseña = linea.strip().split(',')
+            if usuario == gmail:
+                return True
+        return False
+    else:
+        print("Error: El archivo de usuarios no está abierto.")
 
 
 def menuPrincipal():
-    pass
 
+    global usuarioActual
+
+    linea()
+    print("Hola ",usuarioActual )
+    print("-----Menu Principal-----")
+    print("1. Gestion Platos")
+    print("2. Gestion Mesas")
+    print("3. Gestion pedidos")
+    print("4. Cerras sesion")
+    linea()
+
+    opcion = int(input("¿Que desea hacer?: "))
+
+    while (opcion < 5 and opcion > 0):
+        if opcion == 1:
+            pass
+
+        if opcion == 2:
+            pass
+
+        if opcion == 3:
+            pass
+
+        if opcion == 4:
+            pass
+    return 0
 
 #Platos
 def gestionPlatos():
@@ -152,7 +223,15 @@ def modificarPedido():
 
 #Cerrar sesion
 def cerrarSesion():
-    pass
+    
+    global usuarioActual
+
+    usuarioActual = None
+
+    linea()
+    print("Cerrando sesion")
+    linea()
+    menuInicial()
 
 if __name__ == "__main__":
 
