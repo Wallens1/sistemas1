2814b6a871d5eaae769218d5a49cf24f68cfd6eb
Wallens1 <145782388+Wallens1@users.noreply.github.com>
2023-11-26 15:53:33 -0500

    Update 4
    pandas added

--- proyectoFinal.py
+++ proyectoFinal.py
@@ -1,6 +1,17 @@
 import os
+import pandas as pd
+from tabulate import tabulate
+import tkinter as tk
+import datetime
 
-archivoUsuarios = None
+dataFrameUsers = pd.DataFrame
+dataFramePlatos = pd.DataFrame
+dataFrameMesas = pd.DataFrame
+
+matrizUsuarios = []
+matrizPlatos = []
+matrizMesas = []
+
 usuarioActual = None
 
 
@@ -8,8 +19,6 @@ def linea():
     print("------------------------")
 
 def menuInicial():
-
-    global usuarioActual
 
     print("\n-----Menu Principal-----")
     print("1. Registrarse")
@@ -29,7 +38,9 @@ def menuInicial():
             break
         
         if opcion == 3:
-            break
+            return 0
+        
+        else: break
     return 0
 
 def cargarDatos():
@@ -44,6 +55,8 @@ def cargarDatos():
 def cargarUsuarios():
 
     global archivoUsuarios
+    global matrizUsuarios
+    global dataFrameUsers
 
     nombreArchivo = 'Usuarios.txt'
     rutaArchivo = os.path.join(os.path.dirname(__file__), nombreArchivo)
@@ -53,16 +66,91 @@ def cargarUsuarios():
         archivoUsuarios = open(rutaArchivo, "a")
         print("Se ha creado el archivo usuarios")
     else:
-        archivoUsuarios = open(rutaArchivo, "a+")
+        archivoUsuarios = open(rutaArchivo, "r")
         print("Se cargaron los usuarios con exito")
 
+    for linea in archivoUsuarios:
+        usuarioA, contraseñaA = linea.strip().split(',')
+        userPass = [usuarioA, contraseñaA]
+        matrizUsuarios.append(userPass)
+    dataFrameUsers = pd.DataFrame(matrizUsuarios, columns = ["Users", "Passwords"])
+    print(dataFrameUsers)   
+        
 def cargarPlatos():
-    pass
+
+    global archivoPlatos
+    global matrizPlatos
+    global dataFramePlatos
+
+    nombreArchivo = 'Platos.txt'
+    rutaArchivo = os.path.join(os.path.dirname(__file__), nombreArchivo)
+
+    
+    if not os.path.isfile(rutaArchivo):
+        archivoPlatos = open(rutaArchivo, "a")
+        print("Se ha creado el archivo Platos")
+    else:
+        archivoPlatos = open(rutaArchivo, "r")
+        print("Se cargaron los Platos con exito")
+
+    for linea in archivoPlatos:
+        nombrePlato, precioPlato, descripcionPlato, disponibilidad = linea.strip().split(',')
+        plato = [nombrePlato, precioPlato, descripcionPlato, disponibilidad] 
+        matrizPlatos.append(plato)
+    dataFramePlatos = pd.DataFrame(matrizPlatos, columns = ["Nombre", "Precio", "Descripcion", "Disponibilidad"])
+    print(dataFramePlatos)  
+
+def guardarPlatos(nombre, precio, descripcion, disponibilidad):
+
+    global archivoPlatos
+    global matrizPlatos
+    global dataFramePlatos
+
+    with open(archivoPlatos.name, "a") as f:
+        f.write(f"{nombre},{precio},{descripcion},{disponibilidad}\n")
+        f.flush()
+
+    nuevoPlato = [nombre, precio, descripcion, disponibilidad]
+    matrizPlatos.append(nuevoPlato)
+    dataFramePlatos.loc[len(dataFramePlatos)] = nuevoPlato
 
 def cargarMesas():
-    pass
 
-#Menu inicial
+    global archivoMesas
+    global matrizMesas
+    global dataFrameMesas
+
+    nombreArchivo = 'Mesas.txt'
+    rutaArchivo = os.path.join(os.path.dirname(__file__), nombreArchivo)
+
+    
+    if not os.path.isfile(rutaArchivo):
+        archivoMesas = open(rutaArchivo, "a")
+        print("Se ha creado el archivo Mesas")
+    else:
+        archivoMesas = open(rutaArchivo, "r")
+        print("Se cargaron las Mesas con exito")
+
+    for linea in archivoMesas:
+        fecha, noPersonas = linea.strip().split(',')
+        mesa = [fecha, noPersonas] 
+        matrizMesas.append(mesa)
+    dataFrameMesas = pd.DataFrame(matrizMesas, columns = ["Fecha y hora", "NumeroPersonas"])
+    print(dataFrameMesas)  
+
+def guardarMesas(fecha, noPersonas):
+    global archivoMesas
+    global matrizMesas
+    global dataFrameMesas
+
+    with open(archivoMesas.name, "a") as f:
+        f.write(f"{fecha},{noPersonas}\n")
+        f.flush()
+
+    nuevaMesa = [fecha, noPersonas]
+    matrizPlatos.append(nuevaMesa)
+    dataFrameMesas.loc[len(dataFrameMesas)] = nuevaMesa
+    
 def registrarse():
     
     gmail = input("Ingrese su correo: ")
@@ -83,9 +171,7 @@ def registrarse():
     else:
         print("Este correo ya esta registrado, intentelo de nuevo")
         registrarse()
-            
-
-    
+              
 def verificarCorreo(gmail):
 
     mailsValidos = ["gmail", "hotmail", "yahoo", "outlook", "correunivalle"]
@@ -128,34 +214,26 @@ def iniciarSesion():
     
 def confirmarContraseña(usuario, contraseña):
 
-    global archivoUsuarios
+    global dataFrameUsers
 
-    if archivoUsuarios:
-        if(buscarUsuario(usuario)):
-            archivoUsuarios.seek(0)
-            for linea in archivoUsuarios:
-                usuarioA, contraseñaA = linea.strip().split(',')
-                if contraseña == contraseñaA and usuario == usuarioA:
-                    return True
-            return False
-        else:
-            return False
+    fila = dataFrameUsers.loc[dataFrameUsers["Users"] == usuario]
+
+    contraseñaUsuario = fila["Passwords"].values[0]
+    if contraseña == contraseñaUsuario:
+        return True
     else:
-        print("Error: El archivo de usuarios no está abierto.")
+        return False
 
 def buscarUsuario(usuario):
     
-    global archivoUsuarios
+    global dataFrameUsers
 
-    if archivoUsuarios:
-        archivoUsuarios.seek(0)
-        for linea in archivoUsuarios:
-            gmail, contraseña = linea.strip().split(',')
-            if usuario == gmail:
-                return True
-        return False
+    existe = usuario in dataFrameUsers["Users"].values
+
+    if (existe):
+        return True
     else:
-        print("Error: El archivo de usuarios no está abierto.")
+        return False
 
 
 def menuPrincipal():
@@ -168,31 +246,67 @@ def menuPrincipal():
     print("1. Gestion Platos")
     print("2. Gestion Mesas")
     print("3. Gestion pedidos")
-    print("4. Cerras sesion")
+    print("4. Cerrar sesion")
     linea()
 
     opcion = int(input("¿Que desea hacer?: "))
 
     while (opcion < 5 and opcion > 0):
         if opcion == 1:
-            pass
+            gestionPlatos()
+            break
 
         if opcion == 2:
-            pass
+            gestionMesas()
+            break
 
         if opcion == 3:
-            pass
+            gestionPedidos()
 
         if opcion == 4:
-            pass
+            cerrarSesion()
+            menuInicial()
     return 0
 
 #Platos
 def gestionPlatos():
-    pass
+    print("\n-----Gestion de platos-----")
+    print("1. Agregar")
+    print("2. Eliminar")
+    print("3. Actualizar")
+    print("4. Menu principal")
+    print("------------------------")
+
+    opcion = int(input("¿Que desea hacer?: "))
+
+    while (opcion < 5 and opcion > 0):
+        if opcion == 1:
+            agregarPlato()
+            break
+
+        if opcion == 2:
+            eliminarPlato()
+            break
+        
+        if opcion == 3:
+            actualizarPlato()
+            break
+
+        if opcion == 4:
+            menuPrincipal()
+            break
+    return 0
 
 def agregarPlato():
-    pass
+    
+    print("\n-----Agregar Plato-----")
+    nombre = input("Nombre del plato: ")
+    precio = input("Precio del plato: ")
+    descripcion = input("Descripción del plato: ")
+    disponibilidad = input("Disponibilidad del plato (Sí/No): ").upper()
+
+    guardarPlatos(nombre, precio, descripcion, disponibilidad)
+
 def eliminarPlato():
     pass
 def actualizarPlato():
@@ -201,10 +315,75 @@ def actualizarPlato():
 
 #Mesas
 def gestionMesas():
-    pass
+    print("\n-----Gestion de platos-----")
+    print("1. Agregar")
+    print("2. Eliminar")
+    print("3. Actualizar")
+    print("4. Menu principal")
+    print("------------------------")
+
+    opcion = int(input("¿Que desea hacer?: "))
+
+    while (opcion < 5 and opcion > 0):
+        if opcion == 1:
+            agregarReserva()
+            break
+
+        if opcion == 2:
+            eliminarReserva()
+            break
+        
+        if opcion == 3:
+            modificarReserva()
+            break
+
+        if opcion == 4:
+            menuPrincipal()
+            break
+    return 0
 
 def agregarReserva():
-    pass
+    print("\n-----Agregar Reserva-----")
+    
+
+    while(True):
+        try:
+            dia = int(input("Dia: "))
+            if dia > 31 or dia < 1:
+                raise Exception
+            break
+        except Exception:
+            print("El dia debe estar entre 1 y 31")
+    while(True): 
+        try:
+            mes = int(input("Mes: "))
+            if mes > 12 or mes < 0:
+                raise Exception
+            break
+        except Exception:
+            print("El mes debe estar entre 1 y 12")
+    while(True):
+        try:
+            hora = int(input("Hora: "))
+            if hora > 24 or hora < 0:
+                raise Exception
+            break
+        except Exception:
+            print("La hora debe estar entre 0 y 24")
+        try:
+            if dia > 30:
+                if mes == 4 or mes == 6 or mes == 9 or mes == 11:
+                    raise Exception
+        except ValueError:
+            print("Dato invalido")
+        except Exception:
+            print("Este mes tiene no tiene tantos dias")
+
+    fecha = datetime.datetime(2023, dia, mes, hora, 0 , 0)
+    nPersonas = int(input("Numero personas: "))
+
+    guardarMesas(fecha, nPersonas)
+
 def eliminarReserva():
     pass
 def modificarReserva():
@@ -212,7 +391,32 @@ def modificarReserva():
 
 #Pedidos
 def gestionPedidos():
-    pass
+    print("\n-----Gestion de platos-----")
+    print("1. Realizar")
+    print("2. Eliminar")
+    print("3. Actualizar")
+    print("4. Menu principal")
+    print("------------------------")
+
+    opcion = int(input("¿Que desea hacer?: "))
+
+    while (opcion < 5 and opcion > 0):
+        if opcion == 1:
+            realizarPedido()
+            break
+
+        if opcion == 2:
+            elimninarPedido()
+            break
+        
+        if opcion == 3:
+            modificarPedido()
+            break
+
+        if opcion == 4:
+            menuPrincipal()
+            break
+    return 0
 
 def realizarPedido():
     pass
@@ -236,7 +440,6 @@ def cerrarSesion():
 if __name__ == "__main__":
 
     cargarDatos()
-
     menuInicial()
 
     if archivoUsuarios:
